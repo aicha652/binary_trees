89bbc63a7dcec26faa9226de895acb880a09fad5
aicha652 <aichaazlf652@gmail.com>
2023-10-31 04:19:08 -0700

    fix task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,6 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		node->n = value;
 		parent->left = node;
+		node->parent = parent;
 		node->left = NULL;
 		node->right = NULL;
 		return (node);
