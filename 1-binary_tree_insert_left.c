69566b8d9a5b65c61a61a5508268bcc51b8da3d3
aicha652 <aichaazlf652@gmail.com>
2023-10-31 06:30:45 -0700

    fix task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,21 +11,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
-
+	node = binary_tree_node(parent, value);
 	if (node == NULL)
 		return (NULL);
 
 	if (parent == NULL)
 		return (NULL);
 
-	
-	node->n = value;
-	node->parent = parent;
-	node->left = parent->left;
-	node->right = NULL;
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+	}
 	parent->left = node;
-	if (node->left != NULL)
-		node->left->parent = node;
 	return (node);
 }
